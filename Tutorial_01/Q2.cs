086e555fa5783ef583c53874f2a3576852e2f45f
Lenovo <Lenovo@ANAND-RAYTHATHA>
2025-07-01 15:42:15 +0530

    Add project files.

---
+++ Tutorial_01/Q2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_01
+{
+    internal class Q2
+    {
+        public static void Main(string[] args)
+        {
+            String  name , dob , addr , city , state , country , mail;
+            int pincode;
+
+            Console.Write("Enter Name :- ");
+            name = Console.ReadLine();
+            Console.Write("Enter DOB :- ");
+            dob = Console.ReadLine();
+            Console.Write("Enter Address :- ");
+            addr = Console.ReadLine();
+            Console.Write("Enter City :- ");
+            city = Console.ReadLine();
+            Console.Write("Enter Pin Code :- ");
+            pincode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter State :- ");
+            state = Console.ReadLine();
+            Console.Write("Enter Country :- ");
+            country = Console.ReadLine();
+            Console.Write("Enter Mail :- ");
+            mail = Console.ReadLine();
+
+            Console.WriteLine("------ Your Details ------\n" +
+                "Name :- "+name+"\nDOB :- "+ dob+ "\nAddress :- "+addr+"\nCiry :- "+city+"\nPincode :- "+pincode+"\nState :- "+state+
+                "\nCountry :- "+country+"\nMail :- "+mail);
+
+
+
+        }
+    }
+}

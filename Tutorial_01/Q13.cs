13e628194dd4e22b762a88201c04614b7ec68b9a
Raythatha-Anand <araythatha048@rku.ac.in>
2025-07-03 15:22:29 +0530

    .

---
+++ Tutorial_01/Q13.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Tutorial_01
+{
+    internal class Q13
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("enter the limit:- ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            Console.Write(a + " " + b + " ");
+            for (int i = 0; i < limit; i++)
+            {
+
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+
+
+            }
+
+        }
+
+    }
+}

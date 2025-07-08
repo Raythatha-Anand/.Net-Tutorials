39c79f1a059919951488da096131b8e3c298fc5b
Raythatha-Anand <araythatha048@rku.ac.in>
2025-07-08 15:10:09 +0530

    Update Q5.cs

--- Tutorial_01/Q5.cs
+++ Tutorial_01/Q5.cs
@@ -8,5 +8,19 @@ namespace Tutorial_01
 {
     internal class Q5
     {
+        public static void Main(stirng[] args)
+        {
+            string[] abc = string[];
+            Console.Write("Enter How many elements you want to Enter :- ");
+            int lim = Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<lim;i++){
+                Console.Write("Enter Element No"+i+" :-")
+                abc[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach(int a in abc){
+                Console.Write(abc);
+            }
+        }
+            
     }
 }

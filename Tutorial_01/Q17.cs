59b561cef6781d228cd9922889267bae2dd8d592
Raythatha-Anand <araythatha048@rku.ac.in>
2025-07-22 14:34:22 +0530

    Add files via upload
    other Tutorials

---
+++ Tutorial_01/Q17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tutorial_01
+{
+    internal class Q17
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Tutorial_01/Q21.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tutorial_1
+{
+    internal class Q21
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of terms: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            int term = 0;
+
+            Console.Write("Series: ");
+
+            for (int i = 1; i <= n; i++)
+            {
+                term = term * 10 + num;
+                sum += term;
+
+                Console.Write(term);
+
+                if (i < n)
+                    Console.Write(" + ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The Sum is : " + sum);
+
+            Console.Read();
+        }
+    }
+}

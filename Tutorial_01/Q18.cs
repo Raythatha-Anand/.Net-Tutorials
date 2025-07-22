ca1cc863e4bed5087c89aa8c5cdf5b1ab4e1c010
Raythatha-Anand <araythatha048@rku.ac.in>
2025-07-22 14:36:56 +0530

    .

--- Tutorial_01/Q18.cs
+++ Tutorial_01/Q18.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 
 namespace Tutorial_01
@@ -19,3 +20,26 @@ namespace Tutorial_01
         }
     }
 }
+=======
+﻿using System;
+
+namespace Tutorial_01
+{
+    internal class Q18
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(count + " ");
+                    count++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+>>>>>>> 59b561cef6781d228cd9922889267bae2dd8d592
